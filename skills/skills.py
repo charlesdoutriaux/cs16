e1bd45816181f8d7f21c750f13070cc942d9903d
Charles Doutriaux <charles@leptitlilou.com>
2017-01-25 23:44:58 -0800

    ok got something working

--- skills/skills.py
+++ skills/skills.py
@@ -12,7 +12,8 @@ if sys.argv[-1] != "build":
     sys.argv.append("build")
 
 # C extension code
-code = """double add(float a, float b) {
+code = """
+double add(float a, float b) {
    return a+b;
    }
    
@@ -26,7 +27,7 @@ static PyObject *
   Py_myadd(PyObject *self,PyObject *args)
 {
   double a,b;
-  if (!PyArg_ParseTuple(args,"ff",&a,&b))
+  if (!PyArg_ParseTuple(args,"dd",&a,&b))
     return NULL;
   return Py_BuildValue("f",add(a,b));
 }
@@ -100,10 +101,71 @@ setup (name = "extend",
 
 
 build_dir = glob.glob(os.path.join("build","*"))[0]
-print("BDIR:",build_dir)
 sys.path.append(build_dir)
 import extend
-print("PATH:",sys.path)
 import extend.c
-print("DIR",dir(extend))
-print("DIR C:",dir(extend.c))
+
+class Add(object):
+    __slots__ = [
+            "_language",
+            ]
+    def __init__(self,language="python"):
+        self.language=language
+    @property
+    def language(self):
+        return self._language
+    @language.setter
+    def language(self,value):
+        if not isinstance(value,str):
+            raise ValueError("language must be a string")
+        elif not value.lower() in ["c","python"]:
+            raise ValueError("%s not implemented yet")
+        self._language = value.lower()
+
+    def add(self,a,b):
+        if self.language == "c":
+            return extend.c.add(float(a),float(b))
+        else:
+            return float(a)+float(b)
+
+# some basic tests
+P = Add()
+C = Add("C")
+
+
+
+# Some timing for fun
+import random
+import time
+
+def timeit(Operator,N=1000000):
+    start = time.clock()
+    for i in range(N):
+        a = Operator.add(random.random(),random.random())
+    end = time.clock()
+    print("Using %s it took %f seconds to add %i times" % (Operator.language,end-start,N))
+
+timeit(P)
+timeit(C)
+
+
+import unittest
+
+class TestCase(unittest.TestCase):
+    def test(self):
+        self.assertTrue(P.add(2,3) == 5)
+    def test2(self):
+        print(C.add(2,3))
+        self.assertTrue(C.add(2,3) == 5.)
+    def test3(self):
+        with self.assertRaises(AttributeError):
+            P.bad = 5
+    def test4(self):
+        with self.assertRaises(ValueError):
+            P.language = "Fortran"
+    def test5(self):
+        P.language = "c"
+        self.assertTrue(P.language == "c")
+        P.language = "python"
+sys.argv[1:]=[]
+unittest.main()
